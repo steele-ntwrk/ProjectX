f32b8e11a8ce8296c457fabafe61bcb3b2392963
steele-ntwrk <steele-ntwrk@gmail.com>
2024-03-15 05:37:01 +0000

    new or automated update

--- main.py
+++ main.py
@@ -10,12 +10,17 @@ def define_env(env):
     # Define a macro to get VLANs for a given site
     @env.macro
     def get_vlans_for_site(site_name):
-        headers = {
-            'Authorization': f'Token {env.variables["netbox_api_token"]}',
-            'Accept': 'application/json',
-        }
-        sites_response = requests.get(f"{env.variables['netbox_url']}/api/dcim/sites/?name={site_name}", headers=headers)
-        site_id = sites_response.json()['results'][0]['id']
-
-        vlans_response = requests.get(f"{env.variables['netbox_url']}/api/ipam/vlans/?site_id={site_id}", headers=headers)
-        return vlans_response.json()['results']
+        #headers = {
+        #    'Authorization': f'Token {env.variables["netbox_api_token"]}',
+        #    'Accept': 'application/json',
+        #}
+        #sites_response = requests.get(f"{env.variables['netbox_url']}/api/dcim/sites/?name={site_name}", headers=headers)
+        #site_id = sites_response.json()['results'][0]['id']
+#
+        #vlans_response = requests.get(f"{env.variables['netbox_url']}/api/ipam/vlans/?site_id={site_id}", headers=headers)
+        #return vlans_response.json()['results']
+                # Placeholder data
+        return [
+            {'id': 1, 'name': 'VLAN 1', 'description': 'Example VLAN 1'},
+            {'id': 2, 'name': 'VLAN 2', 'description': 'Example VLAN 2'}
+        ]
