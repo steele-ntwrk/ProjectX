af890d84d689414c714ab4f7412e1c4d6581d4fc
steele-ntwrk <steele-ntwrk@gmail.com>
2024-04-05 10:36:58 +0000

    test macros

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ import requests
 def define_env(env):
 
     # Define global variables
-    env.variables['netbox_url'] = 'http://netbox.com'
-    env.variables['netbox_api_token'] = '0123456789abcdef0123456789abcdef01234567'
+    env.variables['netbox_url'] = 'http://192.168.40.163'
+    env.variables['netbox_api_token'] = 'd9ad849f860d24b653c6a2072fff1afb3d8cf49f'
 
     # Define a macro to get VLANs for a given site
     @env.macro
     def get_vlans_for_site(site_name):
-        #headers = {
-        #    'Authorization': f'Token {env.variables["netbox_api_token"]}',
-        #    'Accept': 'application/json',
-        #}
-        #sites_response = requests.get(f"{env.variables['netbox_url']}/api/dcim/sites/?name={site_name}", headers=headers)
-        #site_id = sites_response.json()['results'][0]['id']
-#
-        #vlans_response = requests.get(f"{env.variables['netbox_url']}/api/ipam/vlans/?site_id={site_id}", headers=headers)
-        #return vlans_response.json()['results']
+        headers = {
+            'Authorization': f'Token {env.variables["netbox_api_token"]}',
+            'Accept': 'application/json',
+        }
+        sites_response = requests.get(f"{env.variables['netbox_url']}/api/dcim/sites/?name={site_name}", headers=headers)
+        site_id = sites_response.json()['results'][0]['id']
+
+        vlans_response = requests.get(f"{env.variables['netbox_url']}/api/ipam/vlans/?site_id={site_id}", headers=headers)
+        return vlans_response.json()['results']
                 # Placeholder data
-        return [
-            {'id': 1, 'name': 'VLAN 1', 'description': 'Example VLAN 1'},
-            {'id': 2, 'name': 'VLAN 2', 'description': 'Example VLAN 2'}
-        ]
+        #return [
+        #    {'id': 1, 'name': 'VLAN 1', 'description': 'Example VLAN 1'},
+        #    {'id': 2, 'name': 'VLAN 2', 'description': 'Example VLAN 2'}
+        #]
